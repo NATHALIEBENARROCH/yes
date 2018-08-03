8cb4667f6d6c3ea096d1f8627e5b6b96ed3522ab
Dominick Rivard <dominick.rivard@quebecormedia.com>
2018-08-03 15:21:50 -0400

    Fixing ssh connection.

--- jenkins.sh
+++ jenkins.sh
@@ -13,15 +13,14 @@ if [[ "v2" == "${BRANCH_NAME}" ]]; then
 	/usr/bin/rsync -azvh en/public apt.enlightns.ca:/srv/apps/staging/beta.jdlabs.co/en
 	/usr/bin/rsync -azvh fr/public apt.enlightns.ca:/srv/apps/staging/beta.jdlabs.co/fr
 	/usr/bin/rsync -azvh beta.jdlabs.co.conf apt.enlightns.ca:/srv/apps/staging/beta.jdlabs.co/
-	ssh apt.enlightns.ca "whoami; sudo cp /srv/apps/staging/beta.jdlabs.co/beta.jdlabs.co.conf /etc/nginx/sites-available/beta.jdlabs.co.conf"
+	ssh apt.enlightns.ca "sudo cp /srv/apps/staging/beta.jdlabs.co/beta.jdlabs.co.conf /etc/nginx/sites-available/beta.jdlabs.co.conf; sudo systemctl reload nginx.service"
 elif [[ "prod" == "${BRANCH_NAME}" ]]; then
 	ssh apt.enlightns.ca "rm -rf /srv/apps/production/www.jdlabs.co/*; mkdir -p /srv/apps/production/www.jdlabs.co/{en,fr}"
 	/usr/bin/rsync -azvh en/public apt.enlightns.ca:/srv/apps/production/www.jdlabs.co/en
 	/usr/bin/rsync -azvh fr/public apt.enlightns.ca:/srv/apps/production/www.jdlabs.co/fr
 	/usr/bin/rsync -azvh www.jdlabs.co.conf apt.enlightns.ca:/srv/apps/production/www.jdlabs.co/
-	ssh apt.enlightns.ca "sudo cp /srv/apps/production/www.jdlabs.co/www.jdlabs.co.conf /etc/nginx/sites-available/www.jdlabs.co.conf"
+	ssh apt.enlightns.ca "sudo cp /srv/apps/production/www.jdlabs.co/www.jdlabs.co.conf /etc/nginx/sites-available/www.jdlabs.co.conf; sudo systemctl reload nginx.service"
 else
-    echo "YOur branch has not been configured for deployment: ${BRANCH_NAME}"
+    echo "Your branch has not been configured for deployment: ${BRANCH_NAME}"
 fi
-ssh apt.enlightns.ca "sudo systemctl reload nginx.service"
 
